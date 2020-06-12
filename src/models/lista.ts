19b1f920cb8dc5f4ecf4ef5dac3dbd057d75e990
Beatriz Ferreira dos Santos <BFSantos@raiadrogasil.com>
2020-06-12 13:45:25 -0300

    adicionando interface lista

---
+++ src/models/lista.ts
@@ -0,0 +1,11 @@
+import mongoose, {Schema, model, Document} from 'mongoose';
+
+interface Lista extends Document{
+    descricao:string
+}
+
+const ListaSchema = new Schema({
+    descricao: String
+});
+
+export default model<Lista>('Lista', ListaSchema);
---
+++ src/routes.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const routes = express.Router();
+
+routes.get('/', );
+
+module.exports = routes
